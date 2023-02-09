b4e8dbe73f9997a4ff82c13de92815ea45768772
Elieser <Elieser@LAPTOP-QLD79SSS>
2023-02-09 11:57:49 -0800

    Add project files.

---
+++ Deliverable (4) _Arrays_Myrtil_Elieser/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Elieser Myrtil
+ * Date: 02/08/2023
+ * The program is responsible for outputing a fibonacci sequence without using user input.
+ */
+
+using System;
+
+namespace Deliverable__4___Arrays_Myrtil_Elieser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 25;
+
+            int[] fibonacci = new int[s];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 2; i < s; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                Console.WriteLine("Fibonacci number(" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
